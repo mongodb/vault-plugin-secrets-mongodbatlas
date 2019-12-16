0eaf705aa89d431e1e7473034389731fbf2946e3
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2019-12-16 12:53:04 -0600

    Remove "greater than" and replace with "equals to"

--- util.go
+++ util.go
@@ -1,15 +1,13 @@
 package mongodbatlas
 
 func isOrgKey(orgID, projectID string) bool {
-	return len(orgID) > 0 && !(len(projectID) > 0)
+	return len(orgID) > 0 && len(orgID) == 0
 }
 
 func isProjectKey(orgID, projectID string) bool {
 	return !(len(orgID) > 0) && len(projectID) > 0
-
 }
 
 func isAssignedToProject(orgID, projectID string) bool {
 	return len(orgID) > 0 && len(projectID) > 0
-
 }
