aa5e8bddc1a27fab9b18f6f4306d0502fc40e5f6
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2019-12-17 12:48:08 -0600

    Fix lapsus brutus when measuring length of project ID

--- util.go
+++ util.go
@@ -1,7 +1,7 @@
 package mongodbatlas
 
 func isOrgKey(orgID, projectID string) bool {
-	return len(orgID) > 0 && len(orgID) == 0
+	return len(orgID) > 0 && len(projectID) == 0
 }
 
 func isProjectKey(orgID, projectID string) bool {
